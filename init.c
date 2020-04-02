00c4cd07d4bde74a76b85c26b3bd997f0b646aa5
Tim Jensen <tim.l.jensen@gmail.com>
2020-04-01 19:02:34 -0500

    Tabs are gross

--- init.c
+++ init.c
@@ -1,56 +1,56 @@
 void main()
 {
-	//INIT WEATHER BEFORE ECONOMY INIT------------------------
-	Weather weather = g_Game.GetWeather();
+    //INIT WEATHER BEFORE ECONOMY INIT------------------------
+    Weather weather = g_Game.GetWeather();
 
-	weather.MissionWeather(false);    // false = use weather controller from Weather.c
+    weather.MissionWeather(false);    // false = use weather controller from Weather.c
 
-	weather.GetOvercast().Set( Math.RandomFloatInclusive(0.02, 0.1), 1, 0);
-	weather.GetRain().Set( 0, 1, 0);
-	weather.GetFog().Set( 0, 1, 0);
+    weather.GetOvercast().Set( Math.RandomFloatInclusive(0.02, 0.1), 1, 0);
+    weather.GetRain().Set( 0, 1, 0);
+    weather.GetFog().Set( 0, 1, 0);
 
-	//INIT ECONOMY--------------------------------------
-	Hive ce = CreateHive();
-	if ( ce )
-		ce.InitOffline();
+    //INIT ECONOMY--------------------------------------
+    Hive ce = CreateHive();
+    if ( ce )
+        ce.InitOffline();
 
-	//DATE RESET AFTER ECONOMY INIT-------------------------
-	int year, month, day, hour, minute;
-	int reset_month = 8, reset_day = 10;
-	GetGame().GetWorld().GetDate(year, month, day, hour, minute);
+    //DATE RESET AFTER ECONOMY INIT-------------------------
+    int year, month, day, hour, minute;
+    int reset_month = 8, reset_day = 10;
+    GetGame().GetWorld().GetDate(year, month, day, hour, minute);
 
-	if ((month == reset_month) && (day < reset_day))
-	{
-		GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-	}
-	else
-	{
-		if ((month == reset_month + 1) && (day > reset_day))
-		{
-			GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-		}
-		else
-		{
-			if ((month < reset_month) || (month > reset_month + 1))
-			{
-				GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
-			}
-		}
-	}
+    if ((month == reset_month) && (day < reset_day))
+    {
+        GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+    }
+    else
+    {
+        if ((month == reset_month + 1) && (day > reset_day))
+        {
+            GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+        }
+        else
+        {
+            if ((month < reset_month) || (month > reset_month + 1))
+            {
+                GetGame().GetWorld().SetDate(year, reset_month, reset_day, hour, minute);
+            }
+        }
+    }
 }
 
 class CustomMission: MissionServer
 {
-	override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
-	{
-		Entity playerEnt;
-		playerEnt = GetGame().CreatePlayer(identity, characterName, pos, 0, "NONE");//Creates random player
-		Class.CastTo(m_player, playerEnt);
+    override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
+    {
+        Entity playerEnt;
+        playerEnt = GetGame().CreatePlayer(identity, characterName, pos, 0, "NONE");//Creates random player
+        Class.CastTo(m_player, playerEnt);
 
-		GetGame().SelectPlayer(identity, m_player);
+        GetGame().SelectPlayer(identity, m_player);
 
-		return m_player;
-	}
+        return m_player;
+    }
 
     void StartFedAndWatered(PlayerBase player)
     {
@@ -58,25 +58,25 @@ class CustomMission: MissionServer
         player.GetStatEnergy().Set(player.GetStatEnergy().GetMax());
     }
 
-	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
-	{
-		EntityAI itemTop;
-		EntityAI itemEnt;
-		ItemBase itemBs;
+    override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
+    {
+        EntityAI itemTop;
+        EntityAI itemEnt;
+        ItemBase itemBs;
 
-		itemTop = player.FindAttachmentBySlotName("Body");
+        itemTop = player.FindAttachmentBySlotName("Body");
 
-		if ( itemTop )
-		{
-			itemEnt = itemTop.GetInventory().CreateInInventory("Rag");
-			if ( Class.CastTo(itemBs, itemEnt ) )
-				itemBs.SetQuantity(4);
+        if ( itemTop )
+        {
+            itemEnt = itemTop.GetInventory().CreateInInventory("Rag");
+            if ( Class.CastTo(itemBs, itemEnt ) )
+                itemBs.SetQuantity(4);
 
             itemTop.GetInventory().CreateInInventory("TacticalBaconCan_Opened");
-		}
+        }
 
         StartFedAndWatered(player);
-	}
+    }
 
     override void HandleBody(PlayerBase player)
     {
@@ -87,5 +87,5 @@ class CustomMission: MissionServer
 
 Mission CreateCustomMission(string path)
 {
-	return new CustomMission();
+    return new CustomMission();
 }
