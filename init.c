c2dbf10e60a9e37b6f22bce0619c2888477029ed
Tim Jensen <tim.l.jensen@gmail.com>
2020-04-04 21:07:17 -0500

    Randomize character clothes and weapons

--- init.c
+++ init.c
@@ -41,7 +41,35 @@ void main()
 
 class CustomMission: MissionServer
 {
+    ref array<string> Tops = {
+        "BDUJacket", "Blouse_Green", "BomberJacket_Black", "BomberJacket_Brown",
+        "BomberJacket_Olive", "DenimJacket", "HikingJacket_Black", "HikingJacket_Green",
+        "Hoodie_Black", "Hoodie_Brown", "Hoodie_Green", "HuntingJacket_Brown",
+        "HuntingJacket_Summer", "LabCoat", "M65Jacket_Black", "M65Jacket_Olive",
+        "ParamedicJacket_Green", "PrisonUniformJacket", "QuiltedJacket_Black",
+        "QuiltedJacket_Green", "TacticalShirt_Black", "TacticalShirt_Olive", "WoolCoat_Black"
+    };
+    ref array<string> Bottoms = {
+        "BDUPants", "CargoPants_Black", "CargoPants_Green", "HunterPants_Brown",
+        "HunterPants_Summer", "Jeans_Black", "Jeans_Brown", "Jeans_Green", "ParamedicPants_Green",
+        "PrisonUniformPants", "ShortJeans_Black", "ShortJeans_Brown", "ShortJeans_Green"
+    };
+    ref array<string> Shoes = {
+        "AthleticShoes_Black", "AthleticShoes_Brown", "AthleticShoes_Green", "CombatBoots_Beige",
+        "CombatBoots_Black", "CombatBoots_Brown", "CombatBoots_Green", "CombatBoots_Grey",
+        "HikingBoots_Black", "HikingBoots_Brown", "JungleBoots_Black", "JungleBoots_Brown",
+        "JungleBoots_Green", "JungleBoots_Olive", "MilitaryBoots_Black", "MilitaryBoots_Brown",
+        "Sneakers_Black", "Sneakers_Green", "Wellies_Black", "Wellies_Brown", "Wellies_Green",
+        "WorkingBoots_Brown", "WorkingBoots_Green"
+    };
+    ref array<string> Vests = {
+        "PressVest_Blue", "PressVest_LightBlue", "UKAssVest_Black", "UKAssVest_Camo",
+        "UKAssVest_Khaki", "UKAssVest_Olive"
+    };
+    ref array<string> Belts = {"CivilianBelt", "MilitaryBelt"};
     ref array<string> MP5Handguards = {"MP5_PlasticHndgrd", "MP5_RailHndgrd"};
+    ref array<string> M4Handguards = {"M4_MPHndgrd", "M4_PlasticHndgrd", "M4_RISHndgrd"};
+    ref array<string> M4Buttstocks = {"M4_CQBBttstck", "M4_MPBttstck", "M4_OEBttstck"};
     ref array<string> Knives = {"CombatKnife", "HuntingKnife", "KitchenKnife", "SteakKnife"};
 
     override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
@@ -55,10 +83,26 @@ class CustomMission: MissionServer
         return m_player;
     }
 
-    void StartFedAndWatered(PlayerBase player)
+    void EquipPlayerClothes(PlayerBase player)
     {
-        player.GetStatWater().Set(player.GetStatWater().GetMax());
-        player.GetStatEnergy().Set(player.GetStatEnergy().GetMax());
+        HumanInventory inventory = player.GetHumanInventory();
+        inventory.CreateInInventory(Tops.GetRandomElement());
+        inventory.CreateInInventory(Bottoms.GetRandomElement());
+        inventory.CreateInInventory(Shoes.GetRandomElement());
+        inventory.CreateInInventory(Vests.GetRandomElement());
+        inventory.CreateInInventory(Belts.GetRandomElement());
+        inventory.CreateInInventory("NylonKnifeSheath");
+        // TODO: Put knife in sheath and put sheath in belt
+    }
+
+    void EquipPlayerForSurvival(PlayerBase player)
+    {
+        HumanInventory inventory = player.GetHumanInventory();
+        EntityAI bandage = inventory.CreateInInventory("BandageDressing");
+        inventory.CreateInInventory("TacticalBaconCan_Opened");
+        inventory.CreateInInventory("WaterBottle");
+
+        player.SetQuickBarEntityShortcut(bandage, 3);
     }
 
     EntityAI EquipMP5InHands(HumanInventory inventory)
@@ -76,16 +120,85 @@ class CustomMission: MissionServer
         return mp5;
     }
 
-    EntityAI EquipWinchester(HumanInventory inventory)
+    EntityAI EquipUMPInHands(HumanInventory inventory)
     {
-        EntityAI winchester = inventory.CreateInInventory("Winchester70");
-        winchester.GetInventory().CreateAttachment("HuntingOptic");
+        EntityAI ump = inventory.CreateInHands("UMP45");
+        GameInventory umpInv = ump.GetInventory();
+        umpInv.CreateAttachment("ACOGOptic");
 
-        inventory.CreateInInventory("Ammo_308Win");
-        inventory.CreateInInventory("Ammo_308Win");
-        inventory.CreateInInventory("Ammo_308Win");
+        inventory.CreateInInventory("Mag_UMP_25Rnd");
+        inventory.CreateInInventory("Mag_UMP_25Rnd");
+        inventory.CreateInInventory("Mag_UMP_25Rnd");
 
-        return winchester;
+        return ump;
+    }
+
+    EntityAI EquipM4A1InHands(HumanInventory inventory)
+    {
+        EntityAI m4a1 = inventory.CreateInHands("M4A1");
+        GameInventory m4a1Inv = m4a1.GetInventory();
+        m4a1Inv.CreateAttachment("ACOGOptic");
+        m4a1Inv.CreateAttachment(M4Handguards.GetRandomElement());
+        m4a1Inv.CreateAttachment(M4Buttstocks.GetRandomElement());
+
+        inventory.CreateInInventory("Mag_STANAG_30Rnd");
+        inventory.CreateInInventory("Mag_STANAG_30Rnd");
+        inventory.CreateInInventory("Mag_STANAG_30Rnd");
+
+        return m4a1;
+    }
+
+    EntityAI EquipPrimaryWeaponInHands(HumanInventory inventory)
+    {
+        EntityAI weapon;
+
+        switch (Math.RandomInt(0, 3)) {
+            case 0:
+                weapon = EquipMP5InHands(inventory);
+                break;
+            case 1:
+                weapon = EquipUMPInHands(inventory);
+                break;
+            case 2:
+                weapon = EquipM4A1InHands(inventory);
+                break;
+        }
+
+        return weapon;
+    }
+
+    EntityAI EquipRifle(HumanInventory inventory, string rifleName, string opticName, string ammoName)
+    {
+        EntityAI rifle = inventory.CreateInInventory(rifleName);
+        rifle.GetInventory().CreateAttachment(opticName);
+
+        inventory.CreateInInventory(ammoName);
+        inventory.CreateInInventory(ammoName);
+        inventory.CreateInInventory(ammoName);
+
+        return rifle;
+    }
+
+    EntityAI EquipSecondaryWeapon(HumanInventory inventory)
+    {
+        EntityAI weapon;
+
+        switch (Math.RandomInt(0, 4)) {
+            case 0:
+                weapon = EquipRifle(inventory, "Winchester70", "HuntingOptic", "Ammo_308Win");
+                break;
+            case 1:
+                weapon = EquipRifle(inventory, "B95", "HuntingOptic", "Ammo_308Win");
+                break;
+            case 2:
+                weapon = EquipRifle(inventory, "Mosin9130", "PUScopeOptic", "Ammo_762x54");
+                break;
+            case 3:
+                weapon = EquipRifle(inventory, "SKS", "PUScopeOptic", "Ammo_762x39");
+                break;
+        }
+
+        return weapon;
     }
 
     EntityAI EquipKnife(HumanInventory inventory)
@@ -96,8 +209,8 @@ class CustomMission: MissionServer
     void EquipPlayerWeapons(PlayerBase player)
     {
         HumanInventory inventory = player.GetHumanInventory();
-        EntityAI primary = EquipMP5InHands(inventory);
-        EntityAI secondary = EquipWinchester(inventory);
+        EntityAI primary = EquipPrimaryWeaponInHands(inventory);
+        EntityAI secondary = EquipSecondaryWeapon(inventory);
         EntityAI melee = EquipKnife(inventory);
 
         player.SetQuickBarEntityShortcut(primary, 0);
@@ -105,26 +218,19 @@ class CustomMission: MissionServer
         player.SetQuickBarEntityShortcut(melee, 2);
     }
 
+    void StartFedAndWatered(PlayerBase player)
+    {
+        player.GetStatWater().Set(player.GetStatWater().GetMax());
+        player.GetStatEnergy().Set(player.GetStatEnergy().GetMax());
+    }
+
     override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
     {
-        EntityAI itemTop;
-        EntityAI itemEnt;
-        ItemBase itemBs;
+        player.RemoveAllItems();
 
-        itemTop = player.FindAttachmentBySlotName("Body");
-
-        if (itemTop)
-        {
-            itemEnt = itemTop.GetInventory().CreateInInventory("Rag");
-            if (Class.CastTo(itemBs, itemEnt))
-                itemBs.SetQuantity(4);
-
-            itemTop.GetInventory().CreateInInventory("TacticalBaconCan_Opened");
-            itemTop.GetInventory().CreateInInventory("WaterBottle");
-        }
-
+        EquipPlayerClothes(player);
+        EquipPlayerForSurvival(player);
         EquipPlayerWeapons(player);
-
         StartFedAndWatered(player);
     }
 
