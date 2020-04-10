16f79261ed7c07af8e80ad00f172660f4896ad42
Tim Jensen <tim.l.jensen@gmail.com>
2020-04-09 20:15:52 -0500

    Refactoring how random weapons are chosen and equipped

--- init.c
+++ init.c
@@ -55,9 +55,243 @@ void main()
     SpawnCrate();
 }
 
+class RangedWeapon
+{
+    EntityAI CreateInInventory(HumanInventory inventory)
+    {
+        EntityAI weapon = inventory.CreateInInventory(GetName());
+        return Equip(inventory, weapon);
+    }
+
+    EntityAI CreateInHands(HumanInventory inventory)
+    {
+        EntityAI weapon = inventory.CreateInHands(GetName());
+        return Equip(inventory, weapon);
+    }
+
+    private EntityAI Equip(HumanInventory inventory, EntityAI weapon)
+    {
+        GameInventory weaponInventory = weapon.GetInventory();
+
+        EntityAI optic = CreateAttachment(weaponInventory, GetOptic());
+        if (optic != null)
+        {
+            optic.GetInventory().CreateAttachment("Battery9V");
+        }
+
+        CreateAttachment(weaponInventory, GetHandguard());
+        CreateAttachment(weaponInventory, GetButtstock());
+
+        string ammunition = GetAmmunition();
+        inventory.CreateInInventory(ammunition);
+        inventory.CreateInInventory(ammunition);
+        inventory.CreateInInventory(ammunition);
+
+        return weapon;
+    }
+
+    private EntityAI CreateAttachment(GameInventory inventory, string attachment)
+    {
+        if (attachment == "") return null;
+
+        return inventory.CreateAttachment(attachment);
+    }
+
+    string GetName() {return "";}
+    string GetAmmunition() {return "";}
+    string GetOptic() {return "";}
+    string GetHandguard() {return "";}
+    string GetButtstock() {return "";}
+}
+
+class RandomRangedWeapon extends RangedWeapon
+{
+    static ref TStringArray EMPTY = new TStringArray();
+
+    private TStringArray m_names;
+    private string m_ammunition;
+    private TStringArray m_optics;
+    private TStringArray m_handguards;
+    private TStringArray m_buttstocks;
+
+    void Init(
+        TStringArray names, string ammunition, TStringArray optics = EMPTY,
+        TStringArray handguards = EMPTY, TStringArray buttstocks = EMPTY)
+    {
+        m_names = names;
+        m_optics = optics;
+        m_handguards = handguards;
+        m_buttstocks = buttstocks;
+        m_ammunition = ammunition;
+    }
+
+    string GetName()
+    {
+        return GetRandomElement(m_names);
+    }
+
+    string GetAmmunition()
+    {
+        return m_ammunition;
+    }
+
+    string GetOptic()
+    {
+        return GetRandomElement(m_optics);
+    }
+
+    string GetHandguard()
+    {
+        return GetRandomElement(m_handguards);
+    }
+
+    string GetButtstock()
+    {
+        return GetRandomElement(m_buttstocks);
+    }
+
+    private string GetRandomElement(TStringArray elements)
+    {
+        if (elements == null) return "";
+        if (elements.Count() == 0) return "";
+
+        return elements.GetRandomElement();
+    }
+}
+
+class FixedRangedWeapon extends RangedWeapon
+{
+    string m_name;
+    string m_ammunition;
+    string m_optic;
+    string m_handguard;
+    string m_buttstock;
+
+    void Init(string name, string ammunition, string optic = "", string handguard = "", string buttstock = "")
+    {
+        m_name = name;
+        m_ammunition = ammunition;
+        m_optic = optic;
+        m_handguard = handguard;
+        m_buttstock = buttstock;
+    }
+
+    string GetName()
+    {
+        return m_name;
+    }
+
+    string GetAmmunition()
+    {
+        return m_ammunition;
+    }
+
+    string GetOptic()
+    {
+        return m_optic;
+    }
+
+    string GetHandguard()
+    {
+        return m_handguard;
+    }
+
+    string GetButtstock()
+    {
+        return m_buttstock;
+    }
+}
+
+class Winchester extends FixedRangedWeapon
+{
+    void Winchester()
+    {
+        Init("Winchester70", "Ammo_308Win", "HuntingOptic");
+    }
+}
+
+class Blaze extends FixedRangedWeapon
+{
+    void Blaze()
+    {
+        Init("B95", "Ammo_308Win", "HuntingOptic");
+    }
+}
+
+class Mosin extends FixedRangedWeapon
+{
+    void Mosin()
+    {
+        Init("Mosin9130", "Ammo_762x54", "PUScopeOptic");
+    }
+}
+
+class SKS extends FixedRangedWeapon
+{
+    void SKS()
+    {
+        Init("SKS", "Ammo_762x39", "PUScopeOptic");
+    }
+}
+
+class MP5 extends RandomRangedWeapon
+{
+    static ref TStringArray Names = {"MP5K"};
+    static ref TStringArray Optics = {"M68Optic", "M4_T3NRDSOptic", "ReflexOptic"};
+    static ref TStringArray Handguards = {"MP5_PlasticHndgrd", "MP5_RailHndgrd"};
+    static ref TStringArray Buttstocks = {"MP5k_StockBttstck"};
+
+    void MP5()
+    {
+        Init(Names, "Mag_MP5_30Rnd", Optics, Handguards, Buttstocks);
+    }
+}
+
+class UMP extends RandomRangedWeapon
+{
+    static ref TStringArray Names = {"UMP45"};
+    static ref TStringArray Optics = {"M68Optic", "M4_T3NRDSOptic", "ReflexOptic"};
+
+    void UMP()
+    {
+        Init(Names, "Mag_UMP_25Rnd", Optics);
+    }
+}
+
+class M4A1 extends RandomRangedWeapon
+{
+    static ref TStringArray Names = {"M4A1"};
+    static ref TStringArray Optics = {"M68Optic", "M4_T3NRDSOptic", "ReflexOptic"};
+    static ref TStringArray Handguards = {"M4_MPHndgrd", "M4_PlasticHndgrd", "M4_RISHndgrd"};
+    static ref TStringArray Buttstocks = {"M4_CQBBttstck", "M4_MPBttstck", "M4_OEBttstck"};
+
+    void M4A1()
+    {
+        Init(Names, "Mag_STANAG_30Rnd", Optics, Handguards, Buttstocks);
+    }
+}
+
+class Shotgun extends RandomRangedWeapon
+{
+    static ref TStringArray Names = {"Izh43Shotgun", "Mp133Shotgun"};
+
+    void Shotgun()
+    {
+        Init(Names, "Ammo_12gaPellets");
+    }
+}
+
+class Vaiga extends FixedRangedWeapon
+{
+    void Vaiga()
+    {
+        Init("Saiga", "Mag_Saiga_8Rnd", "", "", "Saiga_Bttstck");
+    }
+}
+
 class CustomMission: MissionServer
 {
-    ref array<string> Tops = {
+    ref TStringArray Tops = {
         "BDUJacket", "Blouse_Green", "BomberJacket_Black", "BomberJacket_Brown",
         "BomberJacket_Olive", "DenimJacket", "HikingJacket_Black", "HikingJacket_Green",
         "Hoodie_Black", "Hoodie_Brown", "Hoodie_Green", "HuntingJacket_Brown",
@@ -65,12 +299,12 @@ class CustomMission: MissionServer
         "ParamedicJacket_Green", "PrisonUniformJacket", "QuiltedJacket_Black",
         "QuiltedJacket_Green", "TacticalShirt_Black", "TacticalShirt_Olive", "WoolCoat_Black"
     };
-    ref array<string> Bottoms = {
+    ref TStringArray Bottoms = {
         "BDUPants", "CargoPants_Black", "CargoPants_Green", "HunterPants_Brown",
         "HunterPants_Summer", "Jeans_Black", "Jeans_Brown", "Jeans_Green", "ParamedicPants_Green",
         "PrisonUniformPants", "ShortJeans_Black", "ShortJeans_Brown", "ShortJeans_Green"
     };
-    ref array<string> Shoes = {
+    ref TStringArray Shoes = {
         "AthleticShoes_Black", "AthleticShoes_Brown", "AthleticShoes_Green", "CombatBoots_Beige",
         "CombatBoots_Black", "CombatBoots_Brown", "CombatBoots_Green", "CombatBoots_Grey",
         "HikingBoots_Black", "HikingBoots_Brown", "JungleBoots_Black", "JungleBoots_Brown",
@@ -78,17 +312,28 @@ class CustomMission: MissionServer
         "Sneakers_Black", "Sneakers_Green", "Wellies_Black", "Wellies_Brown", "Wellies_Green",
         "WorkingBoots_Brown", "WorkingBoots_Green"
     };
-    ref array<string> Vests = {
+    ref TStringArray Vests = {
         "PressVest_Blue", "PressVest_LightBlue", "UKAssVest_Black", "UKAssVest_Camo",
         "UKAssVest_Khaki", "UKAssVest_Olive"
     };
-    ref array<string> Belts = {"CivilianBelt", "MilitaryBelt"};
-    ref array<string> MP5Handguards = {"MP5_PlasticHndgrd", "MP5_RailHndgrd"};
-    ref array<string> M4Handguards = {"M4_MPHndgrd", "M4_PlasticHndgrd", "M4_RISHndgrd"};
-    ref array<string> M4Buttstocks = {"M4_CQBBttstck", "M4_MPBttstck", "M4_OEBttstck"};
-    ref array<string> Knives = {"CombatKnife", "HuntingKnife", "KitchenKnife", "SteakKnife"};
-    ref array<string> NATOPrimaryOptics = {"M68Optic", "M4_T3NRDSOptic", "ReflexOptic"};
-    ref array<string> Shotguns = {"Izh43Shotgun", "Mp133Shotgun"};
+    ref TStringArray Belts = {"CivilianBelt", "MilitaryBelt"};
+    ref TStringArray Knives = {"CombatKnife", "HuntingKnife", "KitchenKnife", "SteakKnife"};
+    autoptr array<ref RangedWeapon> PrimaryWeapons = new array<ref RangedWeapon>();
+    autoptr array<ref RangedWeapon> SecondaryWeapons = new array<ref RangedWeapon>();
+
+    void CustomMission()
+    {
+        PrimaryWeapons.Insert(new MP5());
+        PrimaryWeapons.Insert(new UMP());
+        PrimaryWeapons.Insert(new M4A1());
+        PrimaryWeapons.Insert(new Shotgun());
+        PrimaryWeapons.Insert(new Vaiga());
+
+        SecondaryWeapons.Insert(new Winchester());
+        SecondaryWeapons.Insert(new Blaze());
+        SecondaryWeapons.Insert(new Mosin());
+        SecondaryWeapons.Insert(new SKS());
+    }
 
     override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
     {
@@ -122,134 +367,14 @@ class CustomMission: MissionServer
         player.SetQuickBarEntityShortcut(bandage, 3);
     }
 
-    void AttachNATOPrimaryOpticWithBattery(GameInventory smgInventory)
-    {
-        EntityAI optic = smgInventory.CreateAttachment(NATOPrimaryOptics.GetRandomElement());
-        optic.GetInventory().CreateAttachment("Battery9V");
-    }
-
-    EntityAI EquipMP5InHands(HumanInventory inventory)
-    {
-        EntityAI mp5 = inventory.CreateInHands("MP5K");
-        GameInventory mp5Inv = mp5.GetInventory();
-        AttachNATOPrimaryOpticWithBattery(mp5Inv);
-        mp5Inv.CreateAttachment(MP5Handguards.GetRandomElement());
-        mp5Inv.CreateAttachment("MP5k_StockBttstck");
-
-        inventory.CreateInInventory("Mag_MP5_30Rnd");
-        inventory.CreateInInventory("Mag_MP5_30Rnd");
-        inventory.CreateInInventory("Mag_MP5_30Rnd");
-
-        return mp5;
-    }
-
-    EntityAI EquipUMPInHands(HumanInventory inventory)
-    {
-        EntityAI ump = inventory.CreateInHands("UMP45");
-        AttachNATOPrimaryOpticWithBattery(ump.GetInventory());
-
-        inventory.CreateInInventory("Mag_UMP_25Rnd");
-        inventory.CreateInInventory("Mag_UMP_25Rnd");
-        inventory.CreateInInventory("Mag_UMP_25Rnd");
-
-        return ump;
-    }
-
-    EntityAI EquipM4A1InHands(HumanInventory inventory)
-    {
-        EntityAI m4a1 = inventory.CreateInHands("M4A1");
-        GameInventory m4a1Inv = m4a1.GetInventory();
-        AttachNATOPrimaryOpticWithBattery(m4a1Inv);
-        m4a1Inv.CreateAttachment(M4Handguards.GetRandomElement());
-        m4a1Inv.CreateAttachment(M4Buttstocks.GetRandomElement());
-
-        inventory.CreateInInventory("Mag_STANAG_30Rnd");
-        inventory.CreateInInventory("Mag_STANAG_30Rnd");
-        inventory.CreateInInventory("Mag_STANAG_30Rnd");
-
-        return m4a1;
-    }
-
-    EntityAI EquipShotgunInHands(HumanInventory inventory)
-    {
-        EntityAI shotgun = inventory.CreateInHands(Shotguns.GetRandomElement());
-
-        inventory.CreateInInventory("Ammo_12gaPellets");
-        inventory.CreateInInventory("Ammo_12gaPellets");
-        inventory.CreateInInventory("Ammo_12gaPellets");
-
-        return shotgun;
-    }
-
-    EntityAI EquipSaigaInHands(HumanInventory inventory)
-    {
-        EntityAI saiga = inventory.CreateInHands("Saiga");
-        saiga.GetInventory().CreateAttachment("Saiga_Bttstck");
-
-        inventory.CreateInInventory("Mag_Saiga_8Rnd");
-        inventory.CreateInInventory("Mag_Saiga_8Rnd");
-        inventory.CreateInInventory("Mag_Saiga_8Rnd");
-
-        return saiga;
-    }
-
     EntityAI EquipPrimaryWeaponInHands(HumanInventory inventory)
     {
-        EntityAI weapon;
-
-        switch (Math.RandomInt(0, 5)) {
-            case 0:
-                weapon = EquipMP5InHands(inventory);
-                break;
-            case 1:
-                weapon = EquipUMPInHands(inventory);
-                break;
-            case 2:
-                weapon = EquipM4A1InHands(inventory);
-                break;
-            case 3:
-                weapon = EquipShotgunInHands(inventory);
-                break;
-            case 4:
-                weapon = EquipSaigaInHands(inventory);
-                break;
-        }
-
-        return weapon;
-    }
-
-    EntityAI EquipRifle(HumanInventory inventory, string rifleName, string opticName, string ammoName)
-    {
-        EntityAI rifle = inventory.CreateInInventory(rifleName);
-        rifle.GetInventory().CreateAttachment(opticName);
-
-        inventory.CreateInInventory(ammoName);
-        inventory.CreateInInventory(ammoName);
-        inventory.CreateInInventory(ammoName);
-
-        return rifle;
+        return PrimaryWeapons.GetRandomElement().CreateInHands(inventory);
     }
 
     EntityAI EquipSecondaryWeapon(HumanInventory inventory)
     {
-        EntityAI weapon;
-
-        switch (Math.RandomInt(0, 4)) {
-            case 0:
-                weapon = EquipRifle(inventory, "Winchester70", "HuntingOptic", "Ammo_308Win");
-                break;
-            case 1:
-                weapon = EquipRifle(inventory, "B95", "HuntingOptic", "Ammo_308Win");
-                break;
-            case 2:
-                weapon = EquipRifle(inventory, "Mosin9130", "PUScopeOptic", "Ammo_762x54");
-                break;
-            case 3:
-                weapon = EquipRifle(inventory, "SKS", "PUScopeOptic", "Ammo_762x39");
-                break;
-        }
-
-        return weapon;
+        return SecondaryWeapons.GetRandomElement().CreateInInventory(inventory);
     }
 
     EntityAI EquipKnifeInSheath(EntityAI sheath)
