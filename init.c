1244cd9ae3db89776977d86cdae8583f655018e4
Tim Jensen <tim.l.jensen@gmail.com>
2020-12-20 13:15:05 -0600

    Spawn Christmas tree only once, at the beginning of the first round, and don't destroy it between rounds, because enabling Christmas is a static server setting and therefore won't change from round to round. Also, spawn gift boxes relative to the christmas tree position instead of using absolute coordinates, to make moving the Christmas tree's position easier in the future.

--- init.c
+++ init.c
@@ -72,6 +72,7 @@ class CustomMission extends MissionServer
     static private int COUNTDOWN_DURATION_MS = 10000;
 
     static private const vector LIMBO_POSITON = "7270.39 293.398 2923.94";
+    static private const vector CHRISTMAS_TREE_POSITION = "7343.07 287.052 2614.94";
     static private const vector PLAYAREA_CENTER = "7451 0 2732";
     static private const int CLEANUP_RADIUS = 600;
     static private const int KILL_RADIUS = 570;
@@ -106,6 +107,10 @@ class CustomMission extends MissionServer
         m_settings.load();
 
         game.GetCallQueue(CALL_CATEGORY_GAMEPLAY).CallLater(this.CheckPlayerPositions, 10000, true);
+
+        if (m_settings.christmas) {
+            game.CreateObject("ChristmasTree_Green", CHRISTMAS_TREE_POSITION, false, false, false);
+        }
 
         this.RollForCowboyRound();
         this.CleanupObjectsAndStartRound();
@@ -241,19 +246,17 @@ class CustomMission extends MissionServer
 
         if (m_settings.christmas)
         {
-            game.CreateObject("ChristmasTree_Green", "7343.07 287.052 2614.94", false, false, false);
-
-            GameInventory box1 = ItemBase.Cast(game.CreateObject("GiftBox_Large_1", "7338.07 287.052 2614.94", false, false, false)).GetInventory();
+            GameInventory box1 = ItemBase.Cast(game.CreateObject("GiftBox_Large_1", CHRISTMAS_TREE_POSITION + "-5 0 0", false, false, false)).GetInventory();
             GameInventory megaphone = box1.CreateInInventory("Megaphone").GetInventory();
             megaphone.CreateAttachment("Battery9V");
 
-            GameInventory box2 = ItemBase.Cast(game.CreateObject("GiftBox_Large_2", "7348.07 287.052 2614.94", false, false, false)).GetInventory();
+            GameInventory box2 = ItemBase.Cast(game.CreateObject("GiftBox_Large_2", CHRISTMAS_TREE_POSITION + "5 0 0", false, false, false)).GetInventory();
             box2.CreateInInventory("AK_Suppressor");
             box2.CreateInInventory("M4_Suppressor");
             box2.CreateInInventory("PistolSuppressor");
             box2.CreateInInventory("ImprovisedSuppressor");
 
-            GameInventory box3 = ItemBase.Cast(game.CreateObject("GiftBox_Large_3", "7343.07 287.052 2609.94", false, false, false)).GetInventory();
+            GameInventory box3 = ItemBase.Cast(game.CreateObject("GiftBox_Large_3", CHRISTMAS_TREE_POSITION + "0 0 -5", false, false, false)).GetInventory();
             box3.CreateInInventory("M18SmokeGrenade_Green");
             box3.CreateInInventory("M18SmokeGrenade_Red");
             box3.CreateInInventory("M18SmokeGrenade_Green");
@@ -265,7 +268,7 @@ class CustomMission extends MissionServer
             box3.CreateInInventory("M18SmokeGrenade_Green");
             box3.CreateInInventory("M18SmokeGrenade_Red");
 
-            GameInventory box4 = ItemBase.Cast(game.CreateObject("GiftBox_Large_4", "7343.07 287.052 2619.94", false, false, false)).GetInventory();
+            GameInventory box4 = ItemBase.Cast(game.CreateObject("GiftBox_Large_4", CHRISTMAS_TREE_POSITION + "0 0 5", false, false, false)).GetInventory();
             GameInventory deagle = box4.CreateInInventory("Deagle_Gold").GetInventory();
             deagle.CreateAttachment("PistolOptic");
             deagle.CreateAttachment("PistolSuppressor");
@@ -406,13 +409,6 @@ class CustomMission extends MissionServer
             {
                 Print("Cleaning up " + creature);
                 creature.Delete();
-            }
-
-            ChristmasTree_Green tree = ChristmasTree_Green.Cast(obj);
-            if (tree != null)
-            {
-                Print("Cleaning up " + tree);
-                tree.Delete();
             }
 
             PlayerBase player = PlayerBase.Cast(obj);
